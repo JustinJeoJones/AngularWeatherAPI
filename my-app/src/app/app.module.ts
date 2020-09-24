8252fb1c9d4542932faaed5ce843166f499a0bd9
Jeo <jeo961@gmail.com>
2020-09-24 13:35:56 -0400

    added three blank components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
